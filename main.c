132e2c1562dd212720dce65334c0578e17970bd6
kolio <apxuenuckon@gmail.com>
2016-01-25 00:47:05 +0300

    Всё работает.

--- main.c
+++ main.c
@@ -91,12 +91,39 @@ void error(void){
     delay(50);
     set_all_leds(POWER,0,0);
     delay(100);
-    set_all_leds(0,0,0);    
+    set_all_leds(0,0,0); 
+    delay(10);
 }
+void start_flash(void){
+    unsigned char i,pos=0;
+    
+	while(pos<20){
+        
+        for(i=0;i<LENTA_LENGTH;i++){
+            if(i==pos){
+                led_set_white(70);
+            }else if(i==pos-1){
+                led_set_white(30);
+            }else if(i==pos-2){
+                led_set_white(10);
+            }else if(i==pos-3){
+                led_set_white(5);            
+            }else{
+                led_set_white(0);  
+            }
+        }    
+        pos++;
+        delay(10);
+    }
+}
+
+
+
 void show_color(unsigned char dig){
               switch(dig){
                 case 0:
                  led_set_yellow(POWER);   
+                 led_set_black();
                  led_set_black();
                  led_set_black();
                  led_set_black();
@@ -106,22 +133,47 @@ void show_color(unsigned char dig){
                  led_set_red(POWER);
                  led_set_black();
                  led_set_black();
+                 led_set_black();
                  break;
                 case 2:
                  led_set_black();
                  led_set_black(); 
                  led_set_green(POWER);
                  led_set_black();
+                 led_set_black();
                 case 3:
                  led_set_black();
                  led_set_black();
                  led_set_black(); 
                  led_set_blue(POWER);
+                 led_set_black();
             }  
     
 }
 
+unsigned char wait_button(void){
+    unsigned char button = 255;
+    
+                while(button==255){
+                if(!BUTTON01){
+                    button = 0;
+                }
+                if(!BUTTON02){
+                    button = 1;
+                }
+                if(!BUTTON03){
+                    button = 2;
+                }
+                if(!BUTTON04){
+                    button = 3;
+                }
+            }
+    return button;
+}
 
+
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 
 void main(){
   
@@ -132,11 +184,18 @@ void main(){
     
     setup();
     
-   
-    error();
-   
+    set_all_leds(0,0,0);
+    delay(5);
+    set_all_leds(0,0,0);
+    delay(5);   
+    
+    wait_button();
+    start_flash();
+    g_seed = g_seed;
+    delay(150);
     len = 1;
-  
+    
+
     while(1){
         ///////////////////////////////////////////////
         //   Демонстрация последовательности
@@ -151,44 +210,43 @@ void main(){
                 set_all_leds(0,0,0);
              delay(50);
         }  // Показали Последовательность  
-        len++;
-        delay(150);
-        continue;
+
         /////////////////////////////////////////////////////// 
         //   ВВод и проверка последовательности
         ///////////////////////////////////////////////////////
         Xn_1 = g_seed;
         for(i=0;i<len;i++){
-            button = 255;
-            while(button==255){
-                if(!BUTTON01){
-                    button = 0;
-                }
-                if(!BUTTON02){
-                    button = 1;
-                }
-                if(!BUTTON03){
-                    button = 2;
-                }
-                if(!BUTTON04){
-                    button = 3;
-                }
-            }
+            
             digit = get_next_psevdo_digit(&Xn_1);
-            if(digit!=button){
-                
-                error();
-                break;
+             button = wait_button();
+            if(digit != button){
+                error();            //  Ошибка
+                break;      
+            }else{                  // Ответ верен
+                show_color(digit);  // Индикация правильного ответа
+                delay(100); 
+                set_all_leds(0,0,0);
+                delay(10);
             }
-            
-            
         }//for   
         
-        
-        
-       delay(20); 
+        if(digit == button){        // Всё верно
+            delay(30);          
+            set_all_leds(10,10,10); // Индикация правильного ответы
+            delay(5);
             set_all_leds(0,0,0);
-         delay(20);    
+            delay(100);
+            len++;                  // Увеличение длинны последовательности
+            continue;
+        }else{
+            wait_button();          // Ожидание нажатия для начала следующего раунда
+            start_flash();
+            g_seed = g_seed;        // Захват стартового значения для ПСП
+            delay(10);
+            len = 1;
+        }
+        
+    
     }
 
 
--- ws2811.h
+++ ws2811.h
@@ -4,7 +4,7 @@ void color_set(unsigned char pow);
 //void led_set_white(unsigned char pow);
 void set_all_leds(unsigned char red, unsigned char  green, unsigned char blue);
 
-
+#define led_set_white(pow)  color_set(pow);color_set(pow);color_set(pow);
 #define led_set_green(pow)    color_set(pow);color_set(0);color_set(0);
 #define led_set_red(pow)  color_set(0);color_set(pow);color_set(0);
 #define led_set_blue(pow)   color_set(0);color_set(0);color_set(pow);
